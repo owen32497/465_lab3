da6315509e7b873518aa40e20193aea5837d7f14
njg7716 <36088815+njg7716@users.noreply.github.com>
2018-10-08 14:16:10 -0400

    It works now
    Everything should work

---
+++ DNSEnum.py
@@ -0,0 +1,19 @@
+#This script was written by Nicholas Graca
+import socket
+
+f = input("Please enter the full name of the file with the host names. Make sure it is in CSV format: ");
+hosts_file = open(f, 'r');
+#hosts_file = hosts_file.readlines();
+hosts= []; 
+for line in hosts_file:
+    line = line.replace(" ", "").strip().split(",");
+    hosts = hosts + line;
+
+hosts_file.close();
+
+for host in hosts:
+    try:
+        addr = socket.gethostbyname(host);
+        print(host  + ": " + addr);
+    except:
+        print("There is no IP address for the host: " + host);
