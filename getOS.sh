2daa0cd964f78d4e51cb989c9d8ae2d53d5ef9fb
businessfish <noahmichaels2.0@gmail.com>
2018-10-08 15:54:00 -0400

    main loop written
    netcat output refuses to be stored in a variable, i have no clue
    also netcat crashes when testing port 445, again no clue

--- getOS.sh
+++ getOS.sh
@@ -1,7 +1,23 @@
+#!/bin/sh
 # author        noah michaels
 # assignment    lab 3 csec465
 # date          10/8/2018
 # file          getOS.sh
 
 file=$1
+winport=445
+nixport=22
 
+while read addr; do
+    #check for linux
+    nix="$(nc -nzv $addr $nixport)"
+    test="$(echo "what the fuck")"
+    echo "nix? ${nix}"
+    if [[ "${nix}" == *"(ssh) open"* ]] 
+    then
+        echo "$addr"
+        break
+    fi
+    echo "sdfasdfasdf"
+
+done < $file
