dea1df0c3774cae028a86af6e36d5cfabae38003
njg7716 <36088815+njg7716@users.noreply.github.com>
2018-10-09 15:36:00 -0400

    Comments
    I finished adding in comments adn removed a library that I no longer was using.

--- DNSEnum.py
+++ DNSEnum.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 #This script was written by Nicholas Graca
-import socket, sys
+#it takes a .csv file that has a list of host names as a cmd line argument
+#It will then check to see if that host name has an ip address
+import socket
 
+#this section will get the file name from argv and then read the file
+#It puts the hostnames in the file in a list.
 f = str(sys.argv[1]);
 hosts_file = open(f, 'r');
 hosts= []; 
@@ -9,8 +13,10 @@ for line in hosts_file:
     line = line.replace(" ", "").strip().split(",");
     hosts = hosts + line;
 
+#It is good practice to close the file when you are done reading from it.
 hosts_file.close();
 
+#This section loops through all the host names in the list and checks for their IP
 for host in hosts:
     try:
         addr = socket.gethostbyname(host);
