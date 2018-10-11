53df7373b8f15f68460aa330c9fdd838d0182429
nixs123 <43940154+nixs123@users.noreply.github.com>
2018-10-11 12:06:34 -0400

    Working on the cases in bash

--- Port_Scanner.sh
+++ Port_Scanner.sh
@@ -2,23 +2,21 @@
 #Realized we can only have 1 python script so gonna try to do this in bash
 # file      Port_Scanner
 #!bin/bash
+
 ip=$1
 port=$2
 
-# Take user input for ip, ip range, port or port range
-echo "What would you like to scan?"
-read scan
+#check for cidr
+if [[ip == */*]]; 
+then
+    prips $ip
+fi
 
-# Scan the IP's
-network=scan
-for host in {1..254}; do
-    ping -c1 $network$host &>/dev/null;
-    [ $? -eq 0 ] && echo "$network$host is up"
-done
+#check for ip range
+if [[ip == *-*]];
+then 
+    
+fi
+#check for port range
 
-# Scan the ports
-network=scan
-for port in {1..150}; do 
-    (echo > /dev/tcp/$host/$port) &>/dev/null;
-    [ $? -eq 0 ] && echo "$port open"
-done
+#check for port list
