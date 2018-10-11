af02819e0f933fbf34db050c463869a13f9becc1
nixs123 <43940154+nixs123@users.noreply.github.com>
2018-10-11 10:32:42 -0400

    Try stuff out in bash

---
+++ Port_Scanner.sh
@@ -0,0 +1,14 @@
+#Author     Nicholas Carrigan
+#Realized we can only have 1 python script so gonna try to do this in bash
+# file      Port_Scanner
+#!bin/bash
+ip=$1
+port=$2
+
+# Take user input for ip, ip range, port or port range
+echo "What would you like to scan?"
+read scan
+
+# Scan IPs in the range of 10.10.1.
+
+# Subnet check
