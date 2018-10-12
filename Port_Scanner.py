0044f4815327a0eda97c9d57efa8d46524b7be61
nixs123 <43940154+nixs123@users.noreply.github.com>
2018-10-12 12:49:53 -0400

    Finished Port Scanner and Tested Code

--- Port_Scanner.py
+++ Port_Scanner.py
@@ -1,52 +1,72 @@
 #!/usr/bin/python
-#Nicholas Carrigan
+# Nicholas Carrigan
+# Port_Scanner.py
+
+import struct
 import socket
-import subprocess
+import os
 import sys
-from datetime import datetime
-
-# Ask for input
-host    = raw_input("Enter a remote host to scan: ")
-ip  = socket.gethostbyname(host)
-
-print "Please wait, scanning remote host. This will take around a couple of minutes. Starting Scan of =", ip
-
-
-# Check what time the scan started
-t1 = datetime.now()
-
-# Using the range function to specify ports between 1 and 1024
-#Using the following socket functions in python we are able to find the TCP and UDP options below.
-#SOCK_STREAM
-#Socket type TCP connections
-#SOCK_DGRAM
-#Socket type UDP connections
-try:
-    for port in range(1,1025):  
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = sock.connect_ex((ip, port))
-        if result == 0:
-            print "Port {}: 	 Open".format(port)
-        sock.close()
-
-#random functions stolen online for error debugging
-except socket.gaierror:
-    print 'Hostname could not be resolved. Exiting'
+from netaddr import IPNetwork
+ 
+def tcpScan(ipaddress, ports):
+    try:
+        for ip in ipaddress:
+            print "Scanning ", ip, ":"
+            for port in ports:
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                result = sock.connect_ex((ip, int(port)))
+                if result == 0:
+                    print "Port ", port, " - open"
+                else:
+                    print "Port ", port, " - closed"
+            sock.close()
+ 
+    except KeyboardInterrupt:
+        print "Exiting"
     sys.exit()
-
-except socket.error:
-    print "Couldn't connect to server"
-    sys.exit()
-
-except KeyboardInterrupt:
-    print "You pressed Ctrl+C"
-    sys.exit()
-
-# Recheck time
-t2 = datetime.now()
-
-# Calculates the difference of time for duration of scan
-total =  t2 - t1
-
-# Printing the information to screen
-print 'Scanning Completed. Took: ', total
+ 
+ 
+ 
+def getRange(start, end):
+    ipstruct = struct.Struct('>I')
+    start, = ipstruct.unpack(socket.inet_aton(start))
+    end, = ipstruct.unpack(socket.inet_aton(end))
+    return [socket.inet_ntoa(ipstruct.pack(i)) for i in range(start, end+1)]
+ 
+ 
+def main():
+    # get ip
+    targets = raw_input("Enter the ip address (Single host, Range or CIDR): ")
+    counter = 0      # counter
+    range1 = None      # First ip add
+    range2 = None      # 2nd ip addr or CIDR
+    inputType = 0      # 0 = 1 host, 1 = range, 2 = cidr
+ 
+    for a in targets:
+        if a == '-':
+            range1 = targets[:counter]    #ip start
+            range2 = targets[counter+1:]    #ip end
+            inputType = 1
+            break 
+        if a == '/':
+            inputType = 2
+            range1 = targets
+            break
+        counter= counter + 1
+        range1 = targets 
+    
+    # Obtaining all ips now:
+    ipList = []
+    if inputType == 0:
+        ipList.append(range1)
+ 
+    if inputType == 1:
+        ipList = getRange (range1, range2)
+    
+    if inputType == 2:
+        for ip in IPNetwork(range1):
+           ipList.append(str(ip))
+    ports = raw_input("Enter ports (IE: 20,21,22): ").split(',')
+    tcpScan(ipList, ports)
+ 
+main()
